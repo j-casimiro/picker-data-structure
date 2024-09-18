df6cbf99dfec63019bbe62dd0418a487792b2ff9
ronsdlandjr <147469304+ronsdlandjr@users.noreply.github.com>
2024-09-18 09:48:57 +0800

    Update structure.js

--- structure.js
+++ structure.js
@@ -8,6 +8,139 @@ const structure = {
   product_name: "sample product name",
   price_item: "SAMPLECODEFORITEM123",
   estimated_shipping_date: "08-27-2024",
+  product_line_name: "Flash",
+  brand_item_code: [{
+    "brand_item_code": "PVLMT10",
+        "sizes": [
+          {
+            "master_size_id": 20,
+            "size_name": "X-Small",
+            "size_code": "XS",
+            "gender_category": "Adult"
+          },
+          {
+            "master_size_id": 21,
+            "size_name": "Small",
+            "size_code": "S",
+            "gender_category": "Adult"
+          },
+          {
+            "master_size_id": 22,
+            "size_name": "Medium",
+            "size_code": "M",
+            "gender_category": "Adult"
+          },
+          {
+            "master_size_id": 23,
+            "size_name": "Large",
+            "size_code": "L",
+            "gender_category": "Adult"
+          },
+          {
+            "master_size_id": 24,
+            "size_name": "X-Large",
+            "size_code": "XL",
+            "gender_category": "Adult"
+          },
+          {
+            "master_size_id": 25,
+            "size_name": "2X-Large",
+            "size_code": "2XL",
+            "gender_category": "Adult"
+          },
+          {
+            "master_size_id": 26,
+            "size_name": "3X-Large",
+            "size_code": "3XL",
+            "gender_category": "Adult"
+          },
+          {
+            "master_size_id": 27,
+            "size_name": "4X-Large",
+            "size_code": "4XL",
+            "gender_category": "Adult"
+          },
+          {
+            "master_size_id": 28,
+            "size_name": "5X-Large",
+            "size_code": "5XL",
+            "gender_category": "Adult"
+          }
+        ]
+      },
+      {
+        "brand_item_code": "PVLYT10",
+        "sizes": [
+          {
+            "master_size_id": 13,
+            "size_name": "Youth X-Small",
+            "size_code": "YXS",
+            "gender_category": "Youth"
+          },
+          {
+            "master_size_id": 14,
+            "size_name": "Youth Small",
+            "size_code": "YS",
+            "gender_category": "Youth"
+          },
+          {
+            "master_size_id": 15,
+            "size_name": "Youth Medium",
+            "size_code": "YM",
+            "gender_category": "Youth"
+          },
+          {
+            "master_size_id": 16,
+            "size_name": "Youth Large",
+            "size_code": "YL",
+            "gender_category": "Youth"
+          },
+          {
+            "master_size_id": 17,
+            "size_name": "Youth X-Large",
+            "size_code": "YXL",
+            "gender_category": "Youth"
+          }
+        ]
+  }],
+  decoration_type: {
+    "id": 21,
+    "brand_ids": "[\"55\",\"75\",\"76\",\"49\",\"66\",\"48\",\"47\",\"7\",\"4\",\"63\",\"8\",\"72\",\"3\",\"69\",\"6\",\"5\",\"9\"]",
+    "application_type_id": 16,
+    "image": "https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg",
+    "description": "All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.",
+    "application_method": "[{\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 75, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 55, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 76, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": null, \"label\": null, \"brandId\": 71, \"fileName\": null, \"description\": null}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 49, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 48, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 47, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 7, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 4, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 72, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 3, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 63, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 8, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 66, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Flash items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 69, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": null, \"label\": null, \"brandId\": 53, \"fileName\": null, \"description\": null}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 6, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 5, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"label\": null, \"brandId\": 9, \"fileName\": \"https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/applicationtype_image/20240229210244/KZlYECOusC.jpg\", \"description\": \"All colors, team names, player names, numbers, logos, and patterns are applied through our advanced dye sublimation technology. The dye sublimation process involves printing a garment design onto special sublimation paper. The ink from the printed design is then transferred directly into the top fibers of the garment using heat. Sublimation offers 100% customization—if you can dream it, we can make it. Quickturn items are made the same way as sublimated items but have a shorter production time by utilizing pre-built styles.\"}, {\"image\": null, \"label\": null, \"brandId\": 62, \"fileName\": null, \"description\": null}]",
+    "application_method_id": null
+  },
+  intended_fit: {
+    "id": 32,
+    "brand_id": 5,
+    "fit_intent": "Standard",
+    "description": "The typical fit for each respective sport to maximize comfort and reach high\r\nachieving results.",
+    "image_link": "https://s3.us-west-2.amazonaws.com/qx7/uploaded_files/intendedfit_image/20240815220833/CN28HqOLoR.PNG",
+    "active_status": 1,
+    "gender_id": 13,
+    "brand_id_multiple": "[\"75\",\"55\",\"76\",\"71\",\"49\",\"48\",\"47\",\"4\",\"72\",\"3\",\"63\",\"66\",\"6\",\"5\"]"
+  },
+  master_fabrics: [
+    {
+      "id": 57,
+      "fabric_code": "20074",
+      "weight_oz": 4.42,
+      "content": "100% Polyester",
+      "construction": "Warp Knit"
+    }
+  ],
+  filter_flag_details: {
+    "Cut ?": "Notched Sides",
+    "Sleeve Length": "Sleeveless",
+    "Ply": "2-Ply Reversible",
+    "all_filter_flags": [
+      "Notched Sides",
+      "Sleeveless",
+      "2-Ply Reversible"
+    ]
+  },
   pricing: {
     msrp: {
       Youth: 53.33,
